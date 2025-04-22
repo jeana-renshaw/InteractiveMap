1c3dace80a6fadfa636a6eb50352480cb6fd3364
jeana-renshaw <jrenshaw@sourcepass.com>
2025-04-22 16:40:01 -0400

    Add files via upload

---
+++ map.js
@@ -0,0 +1,30 @@
+// Initialize the map
+var map = L.map('map').setView([37.8, -96], 4);
+
+// Load map tiles from OpenStreetMap
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '© OpenStreetMap contributors'
+}).addTo(map);
+
+// Fetch the state data
+fetch('https://nstny-my.sharepoint.com/personal/jrenshaw_sourcepass_com/_layouts/15/download.aspx?UniqueId=a32c7c1327034d0a871eb2c1d7a1ab03&e=oX6aps')
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(state => {
+      // Draw a shaded rectangle for the state
+      var bounds = L.rectangle(state.coordinates, {
+        color: state.color,
+        weight: 1,
+        fillOpacity: 0.6
+      }).addTo(map);
+
+      // Add tooltip on hover
+      bounds.bindTooltip(state.state + ": " + state.status);
+
+      // Add click event for drill-down details
+      bounds.on('click', function() {
+        alert("State: " + state.state + "\nStatus: " + state.status);
+      });
+    });
+  })
+  .catch(error => console.error("Error loading state data:", error));
